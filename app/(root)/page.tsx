d66de673b7384d2c1811c13e24ca39e822d23891
KingEddie01 <edk6004@gmail.com>
2024-07-02 15:24:09 +0100

    building components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBanlanceBox from '@/components/TotalBanlanceBox'
+
 
 const Home = () => {
+  const loggedIn = {firstName : 'Edmund', lastName : 'Udeh', email: 'edk6004@gmail.com' }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type = 'greeting'
+            title = 'Welcome'
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = 'Access and manage your account efficiently'
+          />
+            <TotalBanlanceBox 
+              accounts = {[]}
+              totalBanks = {1}
+              totalCurrentBalance = {1250.35}
+
+            />
+        </header>
+        RECENT TRANSACTIONS
+        
+
+      </div>
+      <RightSideBar
+        user = {loggedIn}
+        transactions = {[]}
+        banks = {[{currentBalance : 123.50 } ,{ currentBalance : 123.50 }]}
+      />
+    </section>
   )
 }
 
